fa7a5db742de73882a8af89b5c556c9dcd0ab256
uditanshu01 <uprajapati12888@gmail.com>
2026-07-12 10:33:28 +0530

    added trip routes

---
+++ backend/routes/tripRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const tripController = require("../controllers/tripController");
+
+// Get all trips
+router.get("/", tripController.getAllTrips);
+
+// Get trip by ID
+router.get("/:id", tripController.getTripById);
+
+// Create a new trip
+router.post("/", tripController.createTrip);
+
+// Start a trip
+router.put("/:id/start", tripController.startTrip);
+
+// Complete a trip
+router.put("/:id/complete", tripController.completeTrip);
+
+// Cancel a trip
+router.put("/:id/cancel", tripController.cancelTrip);
+
+// Delete trip
+router.delete("/:id", tripController.deleteTrip);
+
+module.exports = router;
